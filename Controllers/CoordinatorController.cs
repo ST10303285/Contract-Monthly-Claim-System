9d8da2b7612520ecab7b4a69ffbe90c366a591d3
ST10303285 <ST10303285@vcconnect.edu.za>
2024-09-10 20:33:57 +0200

    Add project files.

---
+++ Controllers/CoordinatorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contract_Monthly_Claim_System.Controllers
+{
+    public class CoordinatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/LectureController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contract_Monthly_Claim_System.Controllers
+{
+    public class LecturerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitClaim()
+        {
+            return View();
+        }
+    }
+}
